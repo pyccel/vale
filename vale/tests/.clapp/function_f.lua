046e65abdf81d738889542ca2aae4f332c345cb7
ratnania <ahmed.ratnani@ipp.mpg.de>
2017-06-06 12:51:55 +0200

    gitignore

--- vale/tests/.clapp/function_f.lua
+++
@@ -1,6 +0,0 @@
-function f(x, y) 
-  setmetatable(_ENV, { __index=math })  
-  local ky = 2.0
-  local kx = 2.0
-  return ((kx * pi) * (kx * pi) * (kx * pi) * (kx * pi) + (ky * pi) * (ky * pi) * (ky * pi) * (ky * pi))  * sin(kx * pi * x) * sin(ky * pi * y) 
-end 
--- vale/tests/.clapp/function_phi_analytic.lua
+++
@@ -1,6 +0,0 @@
-function phi_analytic(x, y) 
-  setmetatable(_ENV, { __index=math })  
-  local ky = 2.0
-  local kx = 2.0
-  return sin(kx * pi * x) * sin(ky * pi * y) 
-end 
